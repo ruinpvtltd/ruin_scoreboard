af8941b919739ff16b8a6455f51221fb6bd2429f
Ruin Private Limited <135980915+ruinpvtltd@users.noreply.github.com>
2024-04-27 22:54:49 +0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+
+game 'gta5'
+description 'This is a clean and simple scoreboard script with low ms by RUIN PVT LTD'
+lua54 'yes'
+version '1.0.0'
+
+--Client Scripts-- 
+client_scripts {
+ 'Client/*.lua'
+}
+
+
+--Server Scripts-- 
+server_scripts {
+ 'Server/*.lua'
+}
+shared_scripts {
+    'Shared.lua'
+}
+--UI Part-- 
+ui_page {
+ 'html/index.html', 
+}
+
+--File Part-- 
+files {
+ 'html/index.html',
+ 'html/app.js', 
+ 'html/style.css'
+} 
+
+dependencies {
+	'ruin_lib',
+}
